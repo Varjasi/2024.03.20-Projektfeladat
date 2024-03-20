774ba7d1516e33a45f37b0b902b730ce98e28746
Varjasi <teodorvarjasi.puskas@gmail.com>
2024-03-20 12:41:20 +0100

    update 3

--- 1.py
+++ 1.py
@@ -2,6 +2,7 @@
 
 cégadatok = []
 tranzakciók = []
+darabszám = []
 
 with open('adat.txt', 'r', encoding='utf-8') as file:
     for sor in file:
@@ -9,15 +10,35 @@ with open('adat.txt', 'r', encoding='utf-8') as file:
        adat = {'tranzakció:': adatok[0], 'város': adatok[1], 'munkatárs:': adatok[2], 'termékód:': adatok[3], 'darabszám:': adatok[4]}
        cégadatok.append(adat)
        tranzakciók.append(int(adatok[0]))
+       darabszám.append(int(adatok[4]))
 
 print(cégadatok)
-#print(tranzakciók)
 
 #2. feladat
-
-def tranzakciók():
+def tranzakció():
     összeg = 0
     for tranzakciószám in tranzakciók:
         összeg = összeg + tranzakciószám
-        return (összeg)
-tranzakciók()
+
+    print(f'A fájlban található tranzakciók (eladások) száma: {összeg}')
+tranzakció()
+
+#3. feladat
+
+def darabok():
+    összeg = 0
+    for darab in darabszám:
+        összeg = összeg + darab
+
+    print(f'Összes eladott termék száma: {összeg}')
+darabok()
+
+# 4. feladat
+
+sorszám = int(input("Add meg a nap sorszámát"))
+
+while sorszám != '':
+    
+
+    
+
